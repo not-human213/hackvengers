219b90b02836a327ac286baacc4ce7e0247cda7b
not-human213 <111362026+not-human213@users.noreply.github.com>
2023-03-14 01:07:53 +0530

    made efficient

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+#include<string.h>
 using namespace std;
 
 int main()
 {
-    int flag=0;
+    int i=0,flag = 0;
     //using this flag to put the code into while loop to order multiple items.
     int price=0;
     /*using this price var to declare the total money to user and send it to shopkeeper for cash payment later sending it to main file 
     in which the data is being stored. SQL database*/
 
-
-
+    while(flag = 1)
+{
 
     
     
@@ -18,92 +19,24 @@ int main()
  
     int ItNo;
     cin>>ItNo;
-    int ItQty;
+    int ItQty = 1;
     cout<<"Enter the quantity of the item."<<endl;
-    cout<<"x";//using this x to define the quantity, ek hi line me aayega na
+    //cout<<"x";//using this x to define the quantity, ek hi line me aayega na
     cin>>ItQty;
-        int ItVal;
+    int ItVal,z;
 
+    int prices[] = {49,99,59,99,99,75,75,49,25,45,30,10,40,5,99};
+    int size = sizeof(prices)/sizeof(prices[0]);
+    for(z = 0; z < size; z++)
+    {
+        if(ItNo == z+1)
+        {
+            ItVal = prices[z];
+            price = price + (ItVal * ItQty);
+        }
+    }
 
-if(ItNo==1)
-{
-    ItVal=49;
-}
-else if(ItNo==2)
-{
-    ItVal=99;
-}
-else if(ItNo==3)
-{
-    ItVal=59;
-}
-else if(ItNo==4)
-{
-    ItVal=99;
-}
-
-else if(ItNo==5)
-{
-    ItVal=99;
-}
-else if(ItNo==6)
-{
-    ItVal=75;
-}
-else if(ItNo==7)
-{
-    ItVal=75;
-}
-else if(ItNo==8)
-{
-    ItVal=49;
-}
-else if(ItNo==9)
-{
-    ItVal=25;
-}
-else if(ItNo==10)
-{
-    ItVal=45;
-}
-else if(ItNo==11)
-{
-    ItVal=30;
-}
-else if(ItNo==12)
-{
-    ItVal=10;
-}
-else if(ItNo==13)
-{
-    ItVal=40;
-}
-else if(ItNo==14)
-{
-    ItVal=99;
-}
-else if(ItNo==15)
-{
-    ItVal=99;
-}
-
-
-
-
-
-
-
-
-
-   
-   
-   
-   
-   
-
-
-
-
+/*
 
     switch(ItNo){
 case 1:
@@ -194,25 +127,27 @@ default:
 
 
 
-
+*/
 
 
 
     cout<<"Do you want to order something else?"<<endl;
-    int YesNo;
-    cin>>YesNo;
-    if (YesNo=='Y')
+    cin.ignore();
+    string YesNo;
+    getline(cin,YesNo);
+    if (YesNo == "Yes" || YesNo == "yes")
     {
         flag++;
     }
 
     else 
     {
-       
+       // request is send to sk
     }
    
     //THIS IS WHERE TO END THE WHILE LOOP
-    cout<<"Your total is:"<<price;
+    cout<<"Your total is:"<<price<<endl;
+}
 
     return 0;
 
